7dcc8fde86ca561a2c345db2d5f1e30fde8b523c
vlademir guzov <infoman724@gmail.com>
2022-01-05 10:54:16 +0200

    finsih 2.0

--- vigade_otsing_3_gužov/module1.py
+++ vigade_otsing_3_gužov/module1.py
@@ -1,5 +1,8 @@
 from random import *
 def arvud_loendis():
+    """
+    функция спрашивает сколько пользователь хочет сгенерировать значений в списке и в каком диапозоне и выводит результаты на экран:числа в указаном диапазоне и среднее положительных и отрицательных чисел
+    """
     print("Данные:")
     n=abs(int(input("Сколько целых чисел генерируем в список? => ")))
     mini=int(input("Введите минимальное число диапазона => "))
@@ -30,18 +33,39 @@ def arvud_loendis():
     s.sort()
     print(s)
 
-def vahetus(a,b):
+def vahetus(a:int,b:int):
+    """
+    меняет местами значения mini и maxi если mini больше maxi
+    :param int a:первое чилсо
+    :param int b:второе число
+    :rtype:int
+    """
     abi=a
     a=b
     b=abi
     return a,b
 
-def generaator(n,loend,a,b):
+def generaator(n:int,loend:list,a:int,b:int):
+    """
+    выдает n случайных чисел от а до b
+    :param int n:сколько чисел надо с генерировать
+    :param int a:минимальное значение в списке 
+    :param int b:максимальное значение в списке
+    :rtype:int
+    """
     for i in range (n):
         loend.append(randint(a,b))
     
 
-def jagamine(loend,p,n,nol):
+def jagamine(loend:list,p:int,n:int,nol:int):
+    """
+    проверяет и запоминает количество позитивных,негатиных и нулевых значений из списка "loend"
+    :param list loend:список чьи значения проверяются
+    :param int p:количество позитивных чисел в списке
+    :param int n:количество негативных чисел в списке
+    :param int nol:количесвто нулевых значений в списке
+    :rtype:int
+    """
     for el in loend:
         if el>0:
             p.append(el)
@@ -55,7 +79,7 @@ def keskmine(loend):
     """
     Находит среднее значание в указаном списке
     :param list loend:список чьё среднее значение надо найти
-    :rtype:float
+    :rtype:var
     """
     n=len(loend)
     if n==0:
